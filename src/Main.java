5f88dc89ce3da8c336a1c83e4f3345b886e148ef
RuzaikinDY <Devilwind@mail.ru>
2022-04-24 18:07:11 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        int balance=100;
+        int balance = 100;
         //int topup=100;
-        int topup=1100;
-        int newbalance=balance+topup;
-        int bonus=newbalance/100;
-        if (newbalance>=1000) {
+        int topup = 1100;
+        int newbalance = balance + topup;
+        int bonus = newbalance / 100;
+        if (newbalance >= 1000) {
             System.out.println("Бонус: " + bonus + " руб.");
             System.out.println("Общий баланс: " + (newbalance + bonus) + " руб.");
         } else {
